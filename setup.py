27d66a7386641d6ffb41fa7630c473b70928c4eb
conlin-matt <matthew.conlin@noaa.gov>
2024-10-16 14:02:14 +0000

    Add setup info

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Data_Webcam_Synchronizer',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['csv','datetime','matplotlib','moviepy','numpy','pandas','scikit-learn','pytz']
+)
