92c60d172c368acbee60d942d993c357277db9d7
conlin-matt <matthew.conlin@noaa.gov>
2024-10-31 18:22:24 +0000

    Update with new name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Data_Webcam_Synchronizer',
+    name='nwlon_webcoos_synchronizer',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
